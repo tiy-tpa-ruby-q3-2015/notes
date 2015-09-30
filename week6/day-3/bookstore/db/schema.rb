2c6ececd7e018db3896f5f543de4f47dc1a47cd5
Gavin Stark <gavin@theironyard.com>
2015-09-30 10:04:38 -0400

    Week 6 - Day 3 - In class code - adds authors and basic controller and views

--- week6/day-3/bookstore/db/schema.rb
+++ week6/day-3/bookstore/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930132941) do
+ActiveRecord::Schema.define(version: 20150930134631) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "preferred_genre"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
