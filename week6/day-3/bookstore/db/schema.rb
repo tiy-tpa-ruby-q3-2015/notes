3c9594c0d93cbec48daa4e2960399a0fb55aae99
Gavin Stark <gavin@theironyard.com>
2015-09-30 09:39:58 -0400

    Week 6 - Day 3 - In class code - adding a column to the existing books table
    Note all the places we had to edit

--- week6/day-3/bookstore/db/schema.rb
+++ week6/day-3/bookstore/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929143442) do
+ActiveRecord::Schema.define(version: 20150930132941) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150929143442) do
     t.datetime "date_published"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "page_count"
   end
 
 end
