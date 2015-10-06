5f5c92dae76621b109b510811fefa40e05c1865f
Gavin Stark <gavin@theironyard.com>
2015-10-06 12:15:16 -0400

    Creates the user model

--- week7/day-2/bookstore/db/schema.rb
+++ week7/day-2/bookstore/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930142223) do
+ActiveRecord::Schema.define(version: 20151006151958) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20150930142223) do
     t.datetime "updated_at",     null: false
     t.integer  "page_count"
     t.integer  "author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "password_confirmation"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
 end
