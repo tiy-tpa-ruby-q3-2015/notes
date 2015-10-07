71a41c9543a91e211ec32a5014931f95c76be095
Gavin Stark <gavin@theironyard.com>
2015-10-07 11:55:06 -0400

    Adds font awesome stylesheets for social media hotness

--- week7/day-3/bookstore/app/models/user.rb
+++ week7/day-3/bookstore/app/models/user.rb
@@ -6,15 +6,15 @@ class User < ActiveRecord::Base
          :omniauthable, omniauth_providers: [:github]
 
   def self.from_omniauth(auth)
-   user = where(provider: auth['provider'], uid: auth['uid']).first_or_create
+    user = where(provider: auth['provider'], uid: auth['uid']).first_or_create
 
-   user.name         = auth.info.name
-   user.email        = auth.info.email
-   user.nickname     = auth.info.nickname
-   user.access_token = auth.credentials.token
-   user.password     = Devise.friendly_token[0,20]
-   user.save!
+    user.name         = auth.info.name
+    user.email        = auth.info.email
+    user.nickname     = auth.info.nickname
+    user.access_token = auth.credentials.token
+    user.password     = Devise.friendly_token[0,20]
+    user.save!
 
-   return user
+    return user
   end
 end
