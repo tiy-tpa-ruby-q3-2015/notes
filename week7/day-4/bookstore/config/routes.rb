f3fdde1bf644a9f25a9bd425b26c1a26ac5070f8
Gavin Stark <gavin@theironyard.com>
2015-10-08 14:25:04 -0400

    Adds in rails_admin -- note how I make sure the /admin route is only visible by users that are admins inside config/initializers/rails_admin.rb

--- week7/day-4/bookstore/config/routes.rb
+++ week7/day-4/bookstore/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
   root "books#index"
 
